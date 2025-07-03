77b9b42814a86ab89a4ba962ba1c064d5db330b6
Md. Shaifur Rahman Hridoy <srhridoy133@gmail.com>
2025-07-03 23:54:18 +0600

    Problem Solving's

---
+++ Problem_2.dart
@@ -0,0 +1,8 @@
+void main() {
+  String ch = 'e';
+  if ('aeiouAEIOU'.contains(ch)) {
+    print('$ch is a vowel');
+  } else {
+    print('$ch is a consonant');
+  }
+}
---
+++ Problem_8.dart
@@ -0,0 +1,9 @@
+void main() {
+  double num1 = 20;
+  double num2 = 4;
+
+  print('Addition: ${num1 + num2}');
+  print('Subtraction: ${num1 - num2}');
+  print('Multiplication: ${num1 * num2}');
+  print('Division: ${num1 / num2}');
+}
